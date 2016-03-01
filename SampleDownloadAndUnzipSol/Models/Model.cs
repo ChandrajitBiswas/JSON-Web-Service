0a0f17af1f4047c1e118b4e2dac28ed9e91dfd78
Chandrajit <a105165@agl.com.au>
2016-03-02 07:32:41 +1100

    Update source Control

---
+++ SampleDownloadAndUnzipSol/Models/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JSONWebService.Models
+{
+    public class GenderPetList
+    {
+        public List<GenderPet> GenderPets { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Gender { get; set; }
+        public List<Pet> Pets { get; set; }
+    }
+
+    public class GenderPet
+    {
+        public string OwnerGender { get; set; }
+        public string PetType { get; set; }
+        public string PetName { get; set; }
+    }
+
+    public class Pet
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+    }
+}
